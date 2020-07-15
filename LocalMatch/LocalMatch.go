415d209fe84b2ce386d6be0e5e7f56e0ab0969f8
Jerry Xie <hongcheng.xie@my.cityu.edu.hk>
2020-07-15 17:51:15 +0800

    generalize LocalMatch/LocalMatch.go (can control the blockSize in global variable)

--- LocalMatch/LocalMatch.go
+++ LocalMatch/LocalMatch.go
@@ -5,12 +5,22 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/csv"
 	"fmt"
+	"log"
+	"math"
 	"math/big"
+	"os"
 	"strconv"
 
 	"github.com/clearmatics/bn256"
 )
+
+// the struct which stores the original data
+type TestData struct {
+	StockID string
+	Price   int
+}
 
 // the structure of one index item
 type IndexStru struct {
@@ -28,11 +38,52 @@ type QueryStru struct {
 }
 
 var (
-	index [5]IndexStru // the index list
-	g1s   *bn256.G1    // the key of index
-	g2s   *bn256.G2    // the key of query
-	query QueryStru    // the query
+	indexSize      int            = 100                          // the number of indexes
+	blockSize      int            = 2                            // the number of bits in one block
+	fileName       string         = "../../preprocessedList.csv" // the filename of csv file
+	testData       [2000]TestData                                // the original data for our experiment (the top 2000 items)
+	index          []IndexStru    = make([]IndexStru, indexSize) // the index list
+	g1s            *bn256.G1                                     // the key of index
+	g2s            *bn256.G2                                     // the key of query
+	tPLength       int64                                         // the length of array tagPos
+	blockPossValue int64                                         // the number of possible values in one block (2**blockSize)
+	query          QueryStru                                     // the query
 )
+
+// initialize: initialize some basic parameters
+func initialize() {
+	tPLength = int64(math.Exp2(float64(blockSize)) - 1) // the length of array tagPos
+	blockPossValue = tPLength + 1
+}
+
+// keyGene: generate the keys of both index and query (g1s and g2s)
+func keyGene() {
+	s, _ := rand.Int(rand.Reader, bn256.Order)
+	g1s = new(bn256.G1).ScalarBaseMult(s)
+	g2s = new(bn256.G2).ScalarBaseMult(s)
+}
+
+// readCSV(): read the pre-processed csv file
+func readCSV() {
+	var i uint = 0
+	file, _ := os.Open(fileName)
+	reader := csv.NewReader(file)
+	csvContent, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal("Read CSV file error!")
+	}
+	for _, row := range csvContent {
+		if row[0] == "" { // skip the first row (the title)
+			continue
+		}
+		testData[i].StockID = row[1]
+		testData[i].Price, _ = strconv.Atoi(row[2]) // convert the string in csv file to int
+		i++
+		if i >= 2000 { // read the top 2000 items in original data
+			break
+		}
+	}
+}
 
 // getHashedValue: compute the hash value in tag and ciphertext (iStr: the value to be hashed, prefix: the prefix of this block, blockId: the current block number)
 func getHashedValue(iStr string, prefix int64, blockId int) *big.Int {
@@ -61,25 +112,30 @@ func getHashedValue(iStr string, prefix int64, blockId int) *big.Int {
 // indexBlockEnc: encrypt one block index (block: the block value, prefix: the prefix value, id: the current index number, blockId: the current block number)
 func indexBlockEnc(block int64, prefix int64, id int, blockId int) {
 	var i int64
-	var tagPos = [3]int{0, 0, 0}                                     // the current available space of each tag's list
+
+	var tagPos []int = make([]int, tPLength) // the current available space of each tag's list
+	for i = 0; i < tPLength; i++ {
+		tagPos[i] = 0
+	}
+
 	var cipherPos = 0                                                // the current available space of ciphertext list
 	gamma, _ := rand.Int(rand.Reader, bn256.Order)                   // the nonce
 	index[id].pubKey[blockId] = new(bn256.G1).ScalarMult(g1s, gamma) // calculate the public key of one block in one index item
-	for i = 0; i < 3; i++ {                                          // initialize the tag list with 100 (one value out of range)
-		for j := 0; j < 3; j++ {
+	for i = 0; i < tPLength; i++ {                                   // initialize the tag list with 100 (one value out of range)
+		for j := 0; j < int(tPLength); j++ {
 			index[id].tag[blockId][i][j] = 100
 		}
 	}
 	// iStr includes the operator > or <
-	for i = 0; i < 4; i++ {
+	for i = 0; i < blockPossValue; i++ {
 		if i == block { // do not encrypt the equal block
 			continue
 		} else if i < block { // the current variable is smaller than the current block
 			iStr := strconv.FormatInt(i, 10) + "<"       // add the inequality operator into the string to be hashed
 			exp := getHashedValue(iStr, prefix, blockId) // calculate the hash value in tag and ciphertext
 			// calculate the tag
-			tag, _ := strconv.Atoi(new(big.Int).Mod(exp, big.NewInt(3)).String()) // the tag
-			index[id].tag[blockId][tag][tagPos[tag]] = uint8(cipherPos)           // store the list number of the ciphertext in the current available space of corresponding tag
+			tag, _ := strconv.Atoi(new(big.Int).Mod(exp, big.NewInt(tPLength)).String()) // the tag
+			index[id].tag[blockId][tag][tagPos[tag]] = uint8(cipherPos)                  // store the list number of the ciphertext in the current available space of corresponding tag
 			tagPos[tag]++
 
 			// generate the ciphertext
@@ -90,7 +146,7 @@ func indexBlockEnc(block int64, prefix int64, id int, blockId int) {
 			iStr := strconv.FormatInt(i, 10) + ">"
 			exp := getHashedValue(iStr, prefix, blockId)
 			// calculate the tag
-			tag, _ := strconv.Atoi(new(big.Int).Mod(exp, big.NewInt(3)).String()) // the tag
+			tag, _ := strconv.Atoi(new(big.Int).Mod(exp, big.NewInt(tPLength)).String()) // the tag
 			index[id].tag[blockId][tag][tagPos[tag]] = uint8(cipherPos)
 			tagPos[tag]++
 
@@ -108,24 +164,31 @@ func indexItemEnc(v uint32, id int) {
 	vStr := strconv.FormatInt(int64(v), 2) // calculate the binary value
 	vStr = fmt.Sprintf("%032s", vStr)      // pad to 32 bits
 	var prefix int64
-	for i := 0; i < 32/2; i++ {
-		block, _ := strconv.ParseInt(vStr[i*2:i*2+2], 2, 0) // the block contains 2 bits
-		if i == 0 {                                         // the first block (no prefix)
+	for i := 0; i < 32/blockSize; i++ {
+		block, _ := strconv.ParseInt(vStr[i*blockSize:i*blockSize+blockSize], 2, 0) // the block contains 2 bits
+		if i == 0 {                                                                 // the first block (no prefix)
 			prefix = -1
 		} else { // other (has prefix)
-			prefix, _ = strconv.ParseInt(vStr[0:i*2], 2, 0)
+			prefix, _ = strconv.ParseInt(vStr[0:i*blockSize], 2, 0)
 		}
 		indexBlockEnc(block, prefix, id, i) // encrypt the block
 	}
 	index[id].label = v // store the label
 }
 
+// indexEnc: encrypt all the index items
+func indexEnc() {
+	for i := 0; i < indexSize; i++ {
+		indexItemEnc(uint32(testData[i].Price), i)
+	}
+}
+
 // queryBlockEnc: encrypt one block in query, we only consider "larger than" (block: the block value, prefix: the prefix of this block, blockId: the current block number, gamma: gamma of this query)
 func queryBlockEnc(block int64, prefix int64, blockId int, gamma *big.Int) {
-	blockStr := strconv.FormatInt(block, 10) + ">"                             // add the inequality operator
-	exp := getHashedValue(blockStr, prefix, blockId)                           // calculate the hash value
-	tagValue, _ := strconv.Atoi(new(big.Int).Mod(exp, big.NewInt(3)).String()) // calculate the tag
-	query.tag[blockId] = uint8(tagValue)                                       // store the tag in the list
+	blockStr := strconv.FormatInt(block, 10) + ">"                                    // add the inequality operator
+	exp := getHashedValue(blockStr, prefix, blockId)                                  // calculate the hash value
+	tagValue, _ := strconv.Atoi(new(big.Int).Mod(exp, big.NewInt(tPLength)).String()) // calculate the tag
+	query.tag[blockId] = uint8(tagValue)                                              // store the tag in the list
 
 	// calculate the ciphertext of one block
 	query.ciphertext[blockId] = new(bn256.G2).ScalarBaseMult(exp)
@@ -140,12 +203,12 @@ func queryEnc(v uint32) {
 	vStr := strconv.FormatInt(int64(v), 2) // calculate the binary value
 	vStr = fmt.Sprintf("%032s", vStr)      //pad into 32 bits
 	var prefix int64
-	for i := 0; i < 32/2; i++ {
-		block, _ := strconv.ParseInt(vStr[i*2:i*2+2], 2, 0) // the block contains 2 bits
-		if i == 0 {                                         // the first block (no prefix)
+	for i := 0; i < 32/blockSize; i++ {
+		block, _ := strconv.ParseInt(vStr[i*blockSize:i*blockSize+blockSize], 2, 0) // the block contains 2 bits
+		if i == 0 {                                                                 // the first block (no prefix)
 			prefix = -1
 		} else { // other (has prefix)
-			prefix, _ = strconv.ParseInt(vStr[0:i*2], 2, 0)
+			prefix, _ = strconv.ParseInt(vStr[0:i*blockSize], 2, 0)
 		}
 		queryBlockEnc(block, prefix, i, gamma) // encrypt the block
 	}
@@ -153,10 +216,10 @@ func queryEnc(v uint32) {
 
 // match: find the index which the condition holds
 func match() {
-	for i := 0; i < 5; i++ { // scan each index item
+	for i := 0; i < indexSize; i++ { // scan each index item
 		var isMatched bool = false
-		for j := 0; j < 16; j++ { // scan each block
-			for k := 0; k < 3; k++ { // scan all the blocks which their tags are the same as the query's
+		for j := 0; j < 32/blockSize; j++ { // scan each block
+			for k := 0; k < int(tPLength); k++ { // scan all the blocks which their tags are the same as the query's
 				if index[i].tag[j][query.tag[j]][k] == 100 { // if all the aforementioned block are checked
 					break
 				}
@@ -184,21 +247,20 @@ func match() {
 }
 
 func main() {
-	// generate the parameters
-	s, _ := rand.Int(rand.Reader, bn256.Order)
-	g1s = new(bn256.G1).ScalarBaseMult(s)
-	g2s = new(bn256.G2).ScalarBaseMult(s)
+	initialize() // initialize the basic parameters
+	keyGene()    // generate the key set
+	readCSV()    // read the pre-processed csv file
 
-	// add the index items
-	indexItemEnc(500, 0)
-	indexItemEnc(85, 1)
-	indexItemEnc(3, 2)
-	indexItemEnc(150, 3)
-	indexItemEnc(329, 4)
+	// the main procedure
+	fmt.Println("Index Encryption begins")
+	indexEnc() // encrypt the index
+	fmt.Println("Index Encryption done")
 
-	// generate the query
-	queryEnc(500)
+	fmt.Println("Query Encryption begins")
+	queryEnc(uint32(testData[1000].Price)) // generate the query
+	fmt.Println("Query Encryption done")
 
-	// find the matched index items
-	match()
+	fmt.Println("Searching begins")
+	match() // print the list of matched values
+	fmt.Println("Searching Done")
 }
